1928999b4536b5374fed2b779d8dce4fac8604da
Kodi Craft <kodi@kdcf.me>
2023-01-22 19:15:25 +0100

    Initial commit 2

--- target/debug/deps/proc_macros-2c64eb02baa4a974.d
+++
@@ -1,5 +0,0 @@
-/home/kodi/src/drophost/src/proc_macros/target/debug/deps/libproc_macros-2c64eb02baa4a974.so: lib.rs
-
-/home/kodi/src/drophost/src/proc_macros/target/debug/deps/proc_macros-2c64eb02baa4a974.d: lib.rs
-
-lib.rs:
--- target/debug/libproc_macros.d
+++
@@ -1,1 +0,0 @@
-/home/kodi/src/drophost/src/proc_macros/target/debug/libproc_macros.so: /home/kodi/src/drophost/src/proc_macros/lib.rs
